080293d59c9e955e27885228ed75574adaa9a03b
Emanuele Parrinello <parry84@gmail.com>
2020-02-06 14:16:42 +0100

    Add recursive solution to Pangram

--- pangram/src/Pangram.hs
+++ pangram/src/Pangram.hs
@@ -10,14 +10,20 @@ type PangramValue = Int
 type PangramState = (String, PangramValue)
 
 isPangram :: String -> Bool
-isPangram text = check $ evalState (go text) initalState
+isPangram text = isPangramRecursive text initalState
+
+isPangramRecursive [] (_, count) = check count
+isPangramRecursive (x : xs) (letters, count)
+  | check count = True
+  | isAlpha x && isAscii x && (x' `notElem` letters) = isPangramRecursive
+    xs
+    (x' : letters, count + 1)
+  | otherwise = isPangramRecursive xs (letters, count)
+  where x' = toLower x
+
+isPangramMonadic :: String -> Bool
+isPangramMonadic text = check $ evalState (go text) initalState
  where
-  initalState :: PangramState
-  initalState = ("", 0)
-
-  check :: PangramValue -> Bool
-  check = (== 26)
-
   go :: String -> State PangramState PangramValue
   go []       = gets snd
   go (x : xs) = do
@@ -27,3 +33,9 @@ isPangram text = check $ evalState (go text) initalState
     count <- gets snd
     if check count then go [] else go xs
     where x' = toLower x
+
+initalState :: PangramState
+initalState = ("", 0)
+
+check :: PangramValue -> Bool
+check = (== 26)
